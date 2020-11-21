e9a8b1ba8f6f90b56b7eacc0d779b9132079a653
stephen2790022 <toulouse.stephen1@gmail.com>
2020-11-21 17:21:54 +0100

    add devise and navbar

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # , :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+  
+  has_one_attached :avatar
+  validates_presence_of :first_name, :last_name
+end
